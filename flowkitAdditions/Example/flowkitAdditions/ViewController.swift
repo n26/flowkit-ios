e29257841254cb649623e8097f5bfdb2e3a8eca1
Giulio Lombardo <giulio.lombardo@gmail.com>
2022-04-28 12:43:13 +0200

    [FG-7415] Introduce flowkitAdditions pod

---
+++ flowkitAdditions/Example/flowkitAdditions/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  flowkitAdditions
+//
+//  Created by giulio.lombardo@gmail.com on 04/28/2022.
+//  Copyright (c) 2022 giulio.lombardo@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+

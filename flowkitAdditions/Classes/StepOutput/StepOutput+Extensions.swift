cf33db9b1dc7eef59b9f122de78b18b1a46372db
Giulio Lombardo <giulio.lombardo@gmail.com>
2022-04-29 11:03:52 +0200

    [FG-7415] Code review

---
+++ flowkitAdditions/Classes/StepOutput/StepOutput+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  StepOutput+Extension.swift
+//  flowkitAdditions
+//
+//  Created by Giulio Lombardo  on 29/04/22.
+//
+
+import N26FlowKitCore
+
+extension DateInterval: StepOutput { }
+
+extension Decimal: StepOutput { }
+
+extension Double: StepOutput { }
+
+extension Float: StepOutput { }
+
+extension Int: StepOutput { }
+
+extension Set: StepOutput { }

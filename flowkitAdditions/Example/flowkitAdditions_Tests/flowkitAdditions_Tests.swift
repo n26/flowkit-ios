6f2dd6eca84824f8be1e80d5bcf31f1637f9963f
Giulio Lombardo <giulio.lombardo@gmail.com>
2022-04-29 14:59:48 +0200

    [FG-7415] Add FlowKit-ios Tests

---
+++ flowkitAdditions/Example/flowkitAdditions_Tests/flowkitAdditions_Tests.swift
@@ -0,0 +1,11 @@
+//
+//  flowkitAdditions_Tests.swift
+//  flowkitAdditions_Tests
+//
+//  Created by Giulio Lombardo  on 29/04/22.
+//  Copyright © 2022 CocoaPods. All rights reserved.
+//
+
+import XCTest
+
+class flowkitAdditions_Tests: XCTestCase {}
